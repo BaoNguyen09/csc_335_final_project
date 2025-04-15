9f254a6a0b51f99ce5d57e6a6572761f23499278
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-04-15 16:13:07 -0700

    fixing directory issues

---
+++ csc_335_restaurant/src/test/FoodDataTests.java
@@ -0,0 +1,104 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import model.Food;
+import model.FoodData;
+import model.FoodType;
+
+public class FoodDataTests {
+	
+	@Test
+	void testGetQuantity() {
+		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		assertEquals(foodData.getQuantity(), 2);
+	}
+	
+	@Test
+	void testGetModifications() {
+		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		assertEquals(foodData.getModifications(), "None");
+	}
+	
+	@Test
+	void testsetQuantity() {
+		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		foodData.setQuantity(1);
+		assertEquals(foodData.getQuantity(), 1);
+	}
+	
+	@Test
+	void testSetModifications() {
+		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		foodData.setModifications("Add Cheese");
+		assertEquals(foodData.getModifications(), "Add Cheese");
+	}
+	
+	@Test
+	void testGetPrice() {
+		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		assertEquals(foodData.getPrice(), 16.98);
+	}
+	
+	@Test
+	void testToString() {
+		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		assertEquals(foodData.toString(), "FoodData [food=Burger, quantity=2, price: $16.98\nmodifications=None]");
+	}
+	
+	@Test
+	void testHashCode() {
+		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		int hash = 31 + Objects.hash("None", 2);
+		assertEquals(foodData.hashCode(), hash);
+	}
+	
+	@Test
+	void testEqualsSame() {
+		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
+		assertTrue(foodData.equals(foodData));
+	}
+
+	@Test
+	void testEqualsClass() {
+		Food food = new Food("Bread", FoodType.APPETIZER, 4.35);
+		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
+		assertFalse(foodData.equals(food));
+	}
+	
+	@Test
+	void testEqualsFood() {
+		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "add butter");
+		FoodData foodData2 = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
+		assertFalse(foodData.equals(foodData2));
+	}
+	
+	@Test
+	void testEqualsAll() {
+		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
+		FoodData foodData2 = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
+		assertTrue(foodData.equals(foodData2));
+	}
+	
+	@Test
+	void testEqualsQuantity() {
+		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 1, "None");
+		FoodData foodData2 = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
+		assertFalse(foodData.equals(foodData2));
+	}
+	
+	@Test
+	void testEqualsModifications() {
+		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "add butter");
+		FoodData foodData2 = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
+		assertFalse(foodData.equals(foodData2));
+	}
+}
