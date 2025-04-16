424915678adc593932b37cb215c107e945eb0340
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-04-16 16:15:28 -0700

    Update CustomerTests.java

--- csc_335_restaurant/src/test/CustomerTests.java
+++ csc_335_restaurant/src/test/CustomerTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import model.Bill;
 import model.Customer;
+import model.Food;
 import model.FoodType;
 
 class CustomerTests {
@@ -13,7 +14,8 @@ class CustomerTests {
 	@Test
 	void testCost() {
 		Customer person = new Customer("Bob");
-		person.orderFood("Cheese Burger", FoodType.ENTREE, 6.59, 1, "None");
+		Food food = new Food("Cheese Burger", FoodType.ENTREE, 6.59);
+		person.orderFood(food, 1, "None");
 		person.tip(2.60);
 		assertEquals(person.getBillCost(), 9.19);
 	}
@@ -46,7 +48,8 @@ class CustomerTests {
 	@Test
 	void testToString() {
 		Customer person = new Customer("Bob");
-		person.orderFood("Cheese Burger", FoodType.ENTREE, 6.59, 1, "None");
+		Food food = new Food("Cheese Burger", FoodType.ENTREE, 6.59);
+		person.orderFood(food, 1, "None");
 		assertEquals(person.toString(), "Name: Bob, Order: FoodData [food=Cheese Burger, quantity=1, price: $6.59\nmodifications=None]"
 				+ "\nTip: $0.0\nTotal Cost: $6.59\n");
 	}
