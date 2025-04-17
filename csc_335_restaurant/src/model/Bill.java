b694c3c437fa9b51f9267df855cd202d1e6445a8
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-04-16 18:14:48 -0700

    feat: Implement person tate smith (#31)
    * did customer and hopefully fixed the directory issues
    
    * fixing directory issues
    
    * Add files via upload
    
    * Delete csc_335_restaurant/src/model/FoodTests.java
    
    * Delete csc_335_restaurant/src/model/BillTests.java
    
    * Delete csc_335_restaurant/src/model/CustomerTests.java
    
    * Delete csc_335_restaurant/src/model/FoodDataTests.java
    
    * Add files via upload
    
    * Update Bill.java
    
    removed calculate cost private method
    
    * Update Customer.java
    
    * Update CustomerTests.java

--- csc_335_restaurant/src/model/Bill.java
+++ csc_335_restaurant/src/model/Bill.java
@@ -1,5 +1,61 @@
 package model;
 
-public class Bill {
+import java.util.ArrayList;
 
+/*
+ * The Bill class is used to hold information on individual orders. 
+ * Each person has their own bill and this contains everything a person orders
+ * and how much tip they would like to add.
+ */
+
+public class Bill {
+	private double foodCost;
+	private double tip;
+	private ArrayList<FoodData> order;
+	
+	/* The constructor method */
+	public Bill() {
+		order = new ArrayList<FoodData>();
+		tip = 0;
+		foodCost = 0;
+	}
+	
+	/* Copy constructor*/
+	public Bill(Bill other) {
+		this.foodCost = other.foodCost;
+		this.tip = other.tip;
+		ArrayList<FoodData> copyOrder = new ArrayList<>(other.order);
+		this.order = copyOrder;
+	}
+
+	
+	/* This method adds a foodItem to order, it creates a new foodItem and adds it and each time
+	 * this method runs it updates foodCost */
+	public void addFoodItem(FoodData item) {
+		order.add(item);
+		this.foodCost += item.getPrice();
+	}
+	
+	/* Sets the tip */ 
+	public void setTip(double money) {
+		this.tip = money;
+	}
+	
+	/* This method returns the price of the bill which includes the price of the food
+	 * plus the tip*/
+	public double calculateBill() {
+		return foodCost + tip;
+	}
+	
+	@Override
+	public String toString() {
+		String result = "Order: ";
+		for (FoodData item: order) {
+			result += item.toString() + "\n";
+		}
+		result += "Tip: $" + tip + "\n";
+		result += "Total Cost: $" + (foodCost + tip) + "\n";
+		return result;
+	}
+	
 }
--- csc_335_restaurant/src/model/Customer.java
+++ csc_335_restaurant/src/model/Customer.java
@@ -1,5 +1,55 @@
+/* 
+ * This class represents the customer, they each have their own bill objects
+ * so it is easier to split the bill as well as a way to tip, pay the bill,
+ * order food and a few other getters
+ */
+
 package model;
 
 public class Customer {
-
+	private String name;
+	private Bill bill;
+	
+	public Customer(String name) {
+		this.name = name;
+		this.bill = new Bill();
+	}
+	
+	public void tip(double tip) {
+		bill.setTip(tip);
+	}
+	
+	public Bill getBill() {
+		return new Bill(this.bill);
+	}
+	
+	public void payBill() {
+		// when bill is paid the reference is set to null
+		bill = null;
+	}
+	
+	public boolean isBillPaid() {
+		return bill == null;
+	}
+	
+	public double getBillCost() {
+		return bill.calculateBill();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void orderFood(Food food, int quantity, String mods) {
+		/*
+		 * the customer creates a foodData object when they order food which is then passed
+		 * to the bill through this function
+		 */
+		FoodData foodData = new FoodData(food.getName(), food.getType(), food.getPrice(), quantity, mods);
+		bill.addFoodItem(foodData);
+	}
+	
+	public String toString() {
+		return "Name: " + this.name + ", " + this.bill.toString();
+	}
 }
