369b5c99f25bd271c397ffc14b92e251f9a5cbd3
Claire Vu <v.claire47@gmail.com>
2025-04-17 11:30:30 -0700

    test: implemented getter and assign & remove table test

--- csc_335_restaurant/src/test/TableTests.java
+++ csc_335_restaurant/src/test/TableTests.java
@@ -4,11 +4,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import model.Server;
+import model.Table;
+
 class TableTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetters() {
+		Table tableOne = new Table(1, 10);
+		assertTrue(tableOne.assignServer(new Server("Jane")));
+		assertEquals(1, tableOne.getTableNum());
+		assertEquals(10, tableOne.getMaxCapacity());
+		assertEquals("Jane", tableOne.getAssignedServerName());
 	}
+	
+	@Test
+	void testAssignServer() {
+		Table tableOne = new Table(1, 10);
+		assertTrue(tableOne.assignServer(new Server("Jane")));
+		assertEquals("Jane", tableOne.getAssignedServerName());
+		// Cannot assign server to a table with a server
+		assertFalse(tableOne.assignServer(new Server("Bill")));
+	}
+	
+	@Test
+	void testRemoveServer() {
+		Table tableOne = new Table(1, 10);
+		assertTrue(tableOne.assignServer(new Server("Jane")));
+		tableOne.removeServer();
+		assertEquals("", tableOne.getAssignedServerName());
+	}
+	
 
 }
