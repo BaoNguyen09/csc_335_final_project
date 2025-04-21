7641421320301cd9e1552df97cd2735c0ea2e963
rdeleon-bran <rdeleonbran@gmail.com>
2025-04-21 01:15:05 -0700

    Made changes to the sales class
    changed the hashcode and equals method in the foodData class. along with edits to the bill class and sales class.

---
+++ csc_335_restaurant/src/Bill.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.util.ArrayList;
+
+/*
+ * The Bill class is used to hold information on individual orders. 
+ * Each person has their own bill and this contains everything a person orders
+ * and how much tip they would like to add.
+ */
+
+public class Bill {
+	private double foodCost;
+	private double tip;
+	private ArrayList<FoodData> order;
+	
+	/* The constructor method */
+	public Bill() {
+		order = new ArrayList<FoodData>();
+		tip = 0;
+		foodCost = 0;
+	}
+	
+	/* Copy constructor*/
+	public Bill(Bill other) {
+		this.foodCost = other.foodCost;
+		this.tip = other.tip;
+		ArrayList<FoodData> copyOrder = new ArrayList<>(other.order);
+		this.order = copyOrder;
+	}
+
+	
+	/* This method adds a foodItem to order, it creates a new foodItem and adds it and each time
+	 * this method runs it updates foodCost */
+	public void addFoodItem(FoodData item) {
+		order.add(item);
+		calculateFoodCost();
+	}
+	
+	/* Sets the tip */ 
+	public void setTip(double money) {
+		this.tip = money;
+	}
+	
+	/* This method calculates the cost of only the food */
+	private void calculateFoodCost() {
+		double cost = 0;
+		for (FoodData item: order) {
+			cost += item.getPrice();
+		}
+		this.foodCost = cost;
+	}
+	
+	/* This method returns the price of the bill which includes the price of the food
+	 * plus the tip*/
+	public double calculateBill() {
+		return foodCost + tip;
+	}
+	
+	public ArrayList<FoodData> getOrder(){
+		ArrayList<FoodData> copyOrder = new ArrayList<>(this.order);
+		return copyOrder;
+	}
+	
+	@Override
+	public String toString() {
+		String result = "Order: ";
+		for (FoodData item: order) {
+			result += item.toString() + "\n";
+		}
+		result += "Tip: $" + tip + "\n";
+		result += "Total Cost: $" + (foodCost + tip) + "\n";
+		return result;
+	}
+	
+}
---
+++ csc_335_restaurant/src/Sales.java
@@ -0,0 +1,92 @@
+/*
+ * The Sales class keeps track of how many times an item has been ordered using
+ * a HashMap.
+ * */
+package model;
+
+import java.util.*;
+
+
+public class Sales {
+	/* I made sales HashMap String, Integer instead of food, integer because changing
+	 * using food as the key would require changing the hashcode, and equals method
+	 * in FoodData so contains is able to work. Possible to change if we absolutely need to though.
+	 * */
+	private HashMap<FoodData, Integer> sales;
+	private HashMap<FoodData, Double> itemRevenue;
+	
+	// Constructor method
+	public Sales() {
+		sales = new HashMap<FoodData, Integer>();
+		itemRevenue = new HashMap<FoodData, Double>();
+	}
+	
+	
+	/* This method adds all the food in bill to the hashmap by checking the food name
+	 * and adding the quantity
+	 *  */
+	public void addCompletedOrder(Bill bill) {
+		for (FoodData food: bill.getOrder()) {
+			if(!(sales.keySet().contains(food))) {
+				sales.put(food, food.getQuantity());
+				continue;
+			}
+			sales.put(food, sales.get(food) + food.getQuantity());
+		}
+		updateRevenueMap();
+	}
+	
+	// getter method for sales hashmap
+	public HashMap<FoodData, Integer> getSales(){
+		HashMap<FoodData, Integer> copySales = new HashMap<>(this.sales);
+		return copySales;
+	}
+	
+	/* This method is sorted based off the most Sales */
+	public String  sortMostSales(){
+		ArrayList<FoodData> mostSales = new ArrayList<>(sales.keySet());
+		// this code sorts the arrayList by comparing the key based on the value in the
+		// sales hashMap. comparing o2 to o1 makes the list descending order instead of ascending
+		Collections.sort(mostSales, (o1,o2) -> sales.get(o2).compareTo(sales.get(o1)));
+		String result = "Items with Most Sales: \n";
+		for (FoodData item: mostSales) {
+			result += item.getName() + " " + sales.get(item) + "\n";
+		}
+		return result;
+	}
+	
+	/* This method is a helper method that updates the itemRevenue hashmap everytime
+	 * a completed order is added   */
+	private void updateRevenueMap() {
+		for (FoodData item: sales.keySet()) {
+			Double cost = item.getPrice() * sales.get(item);
+			itemRevenue.put(item, cost);
+		}
+		
+	}
+	
+	/* This method will sort the items based on how much money has been made off each
+	 * (most money to least) */
+	public String sortOffRevenue(){
+		String result = "Money Made: \n";
+		// ArrayList full of the keys in itemRevenue
+		ArrayList<FoodData> sortRevenue = new ArrayList<>(itemRevenue.keySet());
+		// sorts items based off the total revenue they have made in descending order
+		Collections.sort(sortRevenue, (o1, o2) -> itemRevenue.get(o2).compareTo(itemRevenue.get(o1)));
+		for (FoodData item: sortRevenue) {
+			result += item.getName() + " $" + itemRevenue.get(item) + "\n";
+		}
+		return result;
+	}
+	
+	@Override 
+	public String toString() {
+		String result = "Item Sales:\n";
+		ArrayList<FoodData> sortedFood = new ArrayList<>(sales.keySet());
+		Collections.sort(sortedFood, (o1, o2) -> o1.getName().compareToIgnoreCase(o2.getName()));
+		for (FoodData food: sortedFood) {
+			result += food.getName() + ": " +  sales.get(food) + "\n";
+		}
+		return result;
+	}
+}
