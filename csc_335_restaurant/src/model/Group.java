875c912b3267fa891301010a6a2053027ef88f62
BaoNguyen09 <nttbvlogs@gmail.com>
2025-04-21 16:48:17 -0700

    feat: add alternative methods for ordering, paying, tipping in Group

--- csc_335_restaurant/src/model/Group.java
+++ csc_335_restaurant/src/model/Group.java
@@ -2,7 +2,9 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * The Group class is used to get information about any groups of customers
@@ -14,12 +16,14 @@ public class Group {
 	private final int groupId; // groupId can only be assigned once when a new Group instance is created
 	private static int nextGroupId = 1;
 	private ArrayList<Customer> members;
+	private Map<String, Customer> customerMap;
 	
 	/* The constructor method */
 	public Group() {
 		this.groupId = nextGroupId;
 		nextGroupId++;
 		this.members = new ArrayList<Customer>();
+		customerMap = new HashMap<String, Customer>();
 	}
 	
 	/* Copy constructor*/
@@ -65,9 +69,41 @@ public class Group {
 	  return Collections.unmodifiableList(sessions);
 	}
 	
+	/*
+	 * This method will handle the ordering directly. Still need to discuss how to
+	 * deal with customer with the same name (maybe add an id)
+	 */
+	public void placeOrder(String name, Food food, int qty, String mods) {
+		Customer customer = customerMap.getOrDefault(name, null);
+		if (customer != null) {
+			customer.orderFood(food, qty, mods);
+		}
+	}
+	
+	/*
+	 * This method will handle the paying bill directly.
+	 */
+	public void payBill(String name) {
+		Customer customer = customerMap.getOrDefault(name, null);
+		if (customer != null) {
+			customer.payBill();
+		}
+	}
+	
+	/*
+	 * This method will handle the tipping directly.
+	 */
+	public void addTip(String name, double amount) {
+		Customer customer = customerMap.getOrDefault(name, null);
+		if (customer != null) {
+			customer.tip(amount);
+		}
+	}
+	
 	public void addPerson(Customer newMember) {
 		if (!members.contains(newMember)) { // only add if new member isn't already in the group
 			members.add(newMember);
+			customerMap.put(newMember.getName(), newMember);
 		}
 	}
 	
