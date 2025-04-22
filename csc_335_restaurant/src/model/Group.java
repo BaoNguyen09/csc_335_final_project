d6612d51c34360d428c6afc7eb3921d12c6a143a
Bao Nguyen <88011047+BaoNguyen09@users.noreply.github.com>
2025-04-22 10:46:23 -0700

    feat: add alternative methods for ordering, paying, tipping in Group (#39)
    * feat: add alternative methods for ordering, paying, tipping in Group
    
    * chore: add reminder

--- csc_335_restaurant/src/model/Group.java
+++ csc_335_restaurant/src/model/Group.java
@@ -2,7 +2,9 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
  * The Group class is used to get information about any groups of customers
@@ -14,18 +16,24 @@ public class Group {
 	private final int groupId; // groupId can only be assigned once when a new Group instance is created
 	private static int nextGroupId = 1;
 	private ArrayList<Customer> members;
+	private Map<String, Customer> customerMap;
 	
 	/* The constructor method */
 	public Group() {
 		this.groupId = nextGroupId;
 		nextGroupId++;
 		this.members = new ArrayList<Customer>();
+		customerMap = new HashMap<String, Customer>();
 	}
 	
 	/* Copy constructor*/
 	public Group(Group other) {
 		this.groupId = other.groupId;
+		// two line below won't work because it tries to access private variables
+		// I put it there as reminder, if we end up needing to use this copy constructor
+		// I will then implement methods to get this list and map
 		this.members = other.members;
+		this.customerMap = other.customerMap;
 	}
 	
 	/* Return total amount of food and tip */
@@ -65,9 +73,41 @@ public class Group {
 	  return Collections.unmodifiableList(sessions);
 	}
 	
+	/*
+	 * This method will handle the ordering directly. Still need to discuss how to
+	 * deal with customer with the same name (maybe add an id)
+	 */
+	public void placeOrder(String name, Food food, int qty, String mods) {
+		Customer customer = customerMap.getOrDefault(name, null);
+		if (customer != null) {
+			customer.orderFood(food, qty, mods);
+		}
+	}
+	
+	/*
+	 * This method will handle the paying bill directly.
+	 */
+	public void payBill(String name) {
+		Customer customer = customerMap.getOrDefault(name, null);
+		if (customer != null) {
+			customer.payBill();
+		}
+	}
+	
+	/*
+	 * This method will handle the tipping directly.
+	 */
+	public void addTip(String name, double amount) {
+		Customer customer = customerMap.getOrDefault(name, null);
+		if (customer != null) {
+			customer.tip(amount);
+		}
+	}
+	
 	public void addPerson(Customer newMember) {
 		if (!members.contains(newMember)) { // only add if new member isn't already in the group
 			members.add(newMember);
+			customerMap.put(newMember.getName(), newMember);
 		}
 	}
 	
