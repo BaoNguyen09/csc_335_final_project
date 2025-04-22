e9a27eade93af312e1312ad833d470b5fd2cf3e4
rdeleon-bran <rdeleonbran@gmail.com>
2025-04-22 10:24:45 -0700

    Update FoodDataTests.java

--- csc_335_restaurant/src/test/FoodDataTests.java
+++ csc_335_restaurant/src/test/FoodDataTests.java
@@ -16,53 +16,53 @@ import model.FoodData;
 import model.FoodType;
 
 public class FoodDataTests {
-	
+
 	@Test
 	void testGetQuantity() {
 		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		assertEquals(foodData.getQuantity(), 2);
 	}
-	
+
 	@Test
 	void testGetModifications() {
 		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		assertEquals(foodData.getModifications(), "None");
 	}
-	
+
 	@Test
 	void testsetQuantity() {
 		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		foodData.setQuantity(1);
 		assertEquals(foodData.getQuantity(), 1);
 	}
-	
+
 	@Test
 	void testSetModifications() {
 		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		foodData.setModifications("Add Cheese");
 		assertEquals(foodData.getModifications(), "Add Cheese");
 	}
-	
+
 	@Test
 	void testGetPrice() {
 		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		assertEquals(foodData.getPrice(), 16.98);
 	}
-	
+
 	@Test
 	void testToString() {
 		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		assertEquals(foodData.toString(), "FoodData [food=Burger, quantity=2, price: $16.98\nmodifications=None]");
 	}
-	
+
 	@Test
 	void testHashCode() {
 		FoodData foodData = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		FoodData foodData1 = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
-		
+
 		assertEquals(foodData.hashCode(), foodData1.hashCode());
 	}
-	
+
 	@Test
 	void testEqualsSame() {
 		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
@@ -75,14 +75,14 @@ public class FoodDataTests {
 		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
 		assertFalse(foodData.equals(food));
 	}
-	
+
 	@Test
 	void testEqualsFood() {
 		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "add butter");
 		FoodData foodData2 = new FoodData("Burger", FoodType.ENTREE, 8.49, 2, "None");
 		assertFalse(foodData.equals(foodData2));
 	}
-	
+
 	@Test
 	void testEqualsAll() {
 		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
@@ -90,5 +90,13 @@ public class FoodDataTests {
 		assertTrue(foodData.equals(foodData2));
 	}
 	
-	
+	@Test
+	void testCopyConstructor() {
+		FoodData foodData = new FoodData("Bread", FoodType.APPETIZER, 4.35, 2, "None");
+		FoodData copyFood = new FoodData(foodData);
+		
+		assertFalse(copyFood == foodData);
+		assertTrue(foodData.equals(copyFood));
+	}
+
 }
