1fa6d06be75695c745a10dfa55c5e92ea00e12ca
rdeleon-bran <rdeleonbran@gmail.com>
2025-04-23 11:26:19 -0700

    Update csc_335_restaurant/src/model/Sales.java
    Co-authored-by: Bao Nguyen <88011047+BaoNguyen09@users.noreply.github.com>

--- csc_335_restaurant/src/model/Sales.java
+++ csc_335_restaurant/src/model/Sales.java
@@ -25,7 +25,7 @@ public class Sales {
 	 *  */
 	public void addCompletedOrder(Bill bill) {
 		for (FoodData food: bill.getOrder()) {
-			Food copyFood = new Food(food.getName(), food.getType(), food.getPrice());
+			Food copyFood = (Food) food;
 			if(!(sales.keySet().contains(copyFood))) {
 				sales.put(copyFood, food.getQuantity());
 				continue;
