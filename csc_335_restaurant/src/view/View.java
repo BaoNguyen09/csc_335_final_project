81800efbbe7c55cb76e0cbbd96f42ac9be4090a8
Claire-Vu <v.claire47@gmail.com>
2025-04-22 17:49:31 -0700

    48 implement basic UI structure (#49)
    * feat: implemented the basic UI structure and design for the restaurant
    
    Note: These are simply placeholders and we will need to go in and change the specific elements to connect with our backend
    
    * refractor: moved the resources folder into the scr package

--- csc_335_restaurant/src/view/View.java
+++ csc_335_restaurant/src/view/View.java
@@ -1,10 +1,36 @@
 package view;
 
+import java.awt.Menu;
+
+import javax.swing.SwingUtilities;
+
+import model.Restaurant;
+import model.Table;
+
 public class View {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Restaurant restaurant = initializeRestaurant();
+		Menu menu = new Menu();
+		
+		
+        SwingUtilities.invokeLater(() -> new RestaurantUI().setVisible(true));
+
 
 	}
 
+	private static Restaurant initializeRestaurant() {
+		Restaurant restaurant = new Restaurant();
+
+        // Table(int tableNum, int capacity)
+//        restaurant.addTable(new Table(1, 10));
+//        restaurant.addTable(new Table(2, 10));
+//        restaurant.addTable(new Table(3, 4));
+//        restaurant.addTable(new Table(4, 4));
+//        restaurant.addTable(new Table(5, 2));
+//        restaurant.addTable(new Table(6, 2));
+
+	
+		return restaurant;
+	}
 }
