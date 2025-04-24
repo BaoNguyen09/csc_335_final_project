8ab83138ef6e0fca9f998a84672816179ebd11ab
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-04-24 15:45:47 -0700

    update controller

--- csc_335_restaurant/src/controller/Controller.java
+++ csc_335_restaurant/src/controller/Controller.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.Restaurant;
 
 public class Controller {
@@ -7,17 +10,35 @@ public class Controller {
 	
 	public Controller(Restaurant restaurant) {
 		this.restaurant = restaurant;
+		restaurant.addServer("Alice");
+		restaurant.addServer("Bob");
+		restaurant.addServer("Charlie");
+		List<String> l1 = new ArrayList<>();
+		l1.add("Bill");
+		l1.add("Allie");
+		l1.add("George");
+		List<String> l2 = new ArrayList<>();
+		l2.add("Chris");
+		l2.add("Tyler");
+		List<String> l3 = new ArrayList<>();
+		l3.add("Sienna");
+		l3.add("Julia");
+		l3.add("Lili");
+		l3.add("John");
+		restaurant.addGroup(l1);
+		restaurant.addGroup(l2);
+		restaurant.addGroup(l3);
 	}
 	
 	public void assignServer(String name, int tableNum) {
-		restaurant.assignServer(name, tableNum);
+		restaurant.assignServerToTable(name, tableNum);
 	}
 	
 	public void assignGroup(int groupNum, int tableNum) {
-		restaurant.assignGroup(groupNum, tableNum);
+		restaurant.assignTable(groupNum, tableNum);
 	}
 	
 	public void removeServer(String name, int tableNum) {
-		restaurant.removeServer(name, tableNum);
+		restaurant.removeServerFromTable(name, tableNum);
 	}
 }
