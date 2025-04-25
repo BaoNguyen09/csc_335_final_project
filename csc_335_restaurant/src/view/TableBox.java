6558638d6b5946567670f56e8e7ac02ad43b3e8a
Claire Vu <v.claire47@gmail.com>
2025-04-24 19:12:40 -0700

    feat: separated tableBox to different UI component

---
+++ csc_335_restaurant/src/view/TableBox.java
@@ -0,0 +1,77 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import model.Restaurant;
+import model.Table;
+
+public class TableBox extends JPanel {
+    private int tableNum;
+    private Restaurant restaurant;
+    private JLabel infoLabel;
+
+    private boolean selected = false;
+
+    public TableBox(int tableNum, Restaurant restaurant) {
+        this.tableNum = tableNum;
+        this.restaurant = restaurant;
+
+        setPreferredSize(new Dimension(120, 100));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        setLayout(new BorderLayout());
+
+        JLabel titleLabel = new JLabel("Table " + tableNum, SwingConstants.CENTER);
+        add(titleLabel, BorderLayout.NORTH);
+
+        infoLabel = new JLabel("", SwingConstants.CENTER);
+        add(infoLabel, BorderLayout.CENTER);
+
+        // Add selection logic
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                selected = !selected;
+                setBorder(BorderFactory.createLineBorder(selected ? Color.BLUE : Color.BLACK, 3));
+                repaint();
+            }
+        });
+
+        refreshStatus();
+    }
+
+    public void refreshStatus() {
+        Table table = restaurant.getTableByNumber(tableNum);
+        if (table == null || !table.isOccupied()) {
+            setBackground(Color.WHITE);
+            infoLabel.setText("<html>Server: None<br>Group: None</html>");
+        } else {
+            String serverName = table.getAssignedServerName();
+            int groupId = table.getGroupId();
+
+            if (table.isOrderTaken()) {
+                setBackground(Color.YELLOW);
+            } else {
+                setBackground(Color.RED);
+            }
+
+            infoLabel.setText("<html>Server: " + (serverName.isEmpty() ? "None" : serverName) +
+                              "<br>Group: " + groupId + "</html>");
+        }
+        repaint();
+    }
+
+    public int getTableNum() {
+        return tableNum;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+}
