2676db1f29237748867081c36bd7bda1ca45a4cc
Tate Smith <128739122+Tate-Smith@users.noreply.github.com>
2025-04-25 23:30:18 -0700

    updated sales

--- csc_335_restaurant/src/model/Sales.java
+++ csc_335_restaurant/src/model/Sales.java
@@ -65,12 +65,27 @@ public class Sales {
 		return copySales;
 	}
 	
-	/* This method is sorted based off the most Sales */
-	public ArrayList<FoodData>  sortMostSales(){
+	/* This method is sorted based off the most Sales most to least*/
+	public ArrayList<FoodData>  sortMostSalesDescending(){
 		ArrayList<Food> mostSales = new ArrayList<>(sales.keySet());
 		// this code sorts the arrayList by comparing the key based on the value in the
 		// sales hashMap.
 		Collections.sort(mostSales, (o1,o2) -> sales.get(o1).compareTo(sales.get(o2)));
+		ArrayList<FoodData> returnList = new ArrayList<>();
+		for (Food item: mostSales) {
+			FoodData copyItem = new FoodData(item.getName(), item.getType(), 
+					item.getPrice(), sales.get(item), "");
+			returnList.add(copyItem);
+		}
+		return returnList;
+	}
+	
+	/* This method is sorted based off the most Sales least to most*/
+	public ArrayList<FoodData>  sortMostSalesAscending(){
+		ArrayList<Food> mostSales = new ArrayList<>(sales.keySet());
+		// this code sorts the arrayList by comparing the key based on the value in the
+		// sales hashMap.
+		Collections.sort(mostSales, (o1,o2) -> sales.get(o2).compareTo(sales.get(o1)));
 		ArrayList<FoodData> returnList = new ArrayList<>();
 		for (Food item: mostSales) {
 			FoodData copyItem = new FoodData(item.getName(), item.getType(), 
@@ -92,11 +107,27 @@ public class Sales {
 	
 	/* This method will sort the items based on how much money has been made off each
 	 * (most money to least) */
-	public ArrayList<FoodData> sortOffRevenue(){
+	public ArrayList<FoodData> sortOffRevenueDescending(){
 		// ArrayList full of the keys in itemRevenue
 		ArrayList<Food> sortRevenue = new ArrayList<>(itemRevenue.keySet());
 		// sorts items based off the total revenue they have made in ascending order
 		Collections.sort(sortRevenue, (o1, o2) -> itemRevenue.get(o1).compareTo(itemRevenue.get(o2)));
+		ArrayList<FoodData> copyList = new ArrayList<>();
+		for (Food item: sortRevenue) {
+			FoodData copyItem = new FoodData(item.getName(), item.getType(), 
+					item.getPrice(), sales.get(item), "");
+			copyList.add(copyItem);
+		}
+		return copyList;
+	}
+	
+	/* This method will sort the items based on how much money has been made off each
+	 * (least money to most) */
+	public ArrayList<FoodData> sortOffRevenueAscending(){
+		// ArrayList full of the keys in itemRevenue
+		ArrayList<Food> sortRevenue = new ArrayList<>(itemRevenue.keySet());
+		// sorts items based off the total revenue they have made in ascending order
+		Collections.sort(sortRevenue, (o1, o2) -> itemRevenue.get(o2).compareTo(itemRevenue.get(o1)));
 		ArrayList<FoodData> copyList = new ArrayList<>();
 		for (Food item: sortRevenue) {
 			FoodData copyItem = new FoodData(item.getName(), item.getType(), 
